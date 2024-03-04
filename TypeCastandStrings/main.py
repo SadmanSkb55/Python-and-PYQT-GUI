3e6514ea676ae5c82f9cdbcafccaf7851c2b0728
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2024-03-04 23:54:22 +0600

    update

--- TypeCastandStrings/main.py
+++ TypeCastandStrings/main.py
@@ -12,3 +12,21 @@ stringh="Sadman's Workstation"
 stringg='is not that good'
 stringf='which is not for "Beginners"'
 print(stringh+' '+stringg+" "+stringf)
+
+strx='''
+hi,
+this is sadman.
+nice to meet you
+
+'''
+print(strx)
+stry='hello pepoll'
+print(stry[3])
+print(stry[-4])
+#print(stry[37])
+print(stry[0:4])
+print(stry[3:])
+print(stry[:6])
+another=stry[:]
+print(another)
+print(stry[1:-1])
