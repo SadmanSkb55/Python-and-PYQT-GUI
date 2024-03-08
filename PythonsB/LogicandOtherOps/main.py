48f54bb70cfa6043051f0059b9e8d40e2f3fde67
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2024-03-08 23:57:11 +0600

    and or not

--- PythonsB/LogicandOtherOps/main.py
+++ PythonsB/LogicandOtherOps/main.py
@@ -14,3 +14,16 @@ if is_cold:
     print("Its cold day")
 print("Enjoy")#should keep in one line like first one
 
+has_high_cgpa=True
+has_good_credit=True
+has_skills=True
+
+if has_high_cgpa and has_good_credit :
+    print("Eligible for TA")
+has_high_cgpa=False
+if has_high_cgpa or has_skills:
+    print("Go for jobs")
+has_high_cgpa=True
+has_skills=False
+if has_high_cgpa and not has_skills:
+    print("Fuck off")
