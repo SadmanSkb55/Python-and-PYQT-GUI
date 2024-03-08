2241fc79d4929420ab0791c6618c55ac64d1d300
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2024-03-08 23:28:53 +0600

    logical if else elif

---
+++ PythonsB/LogicandOtherOps/main.py
@@ -0,0 +1,16 @@
+is_cold=True
+is_hot=True
+is_cold=False
+if is_cold:
+    print("Its cold day")
+    print("Enjoy cold")
+elif is_hot:
+    print("Hot")
+    print("Enjoy hot")
+else:
+    print("Fuck off")
+is_cold=False
+if is_cold:
+    print("Its cold day")
+print("Enjoy")#should keep in one line like first one
+
