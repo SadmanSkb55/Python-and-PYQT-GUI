cc81ffe17d763275187d3505186636e6c0b07908
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2024-03-09 23:52:57 +0600

    for loop

--- PythonsB/Loops/main.py
+++ PythonsB/Loops/main.py
@@ -10,3 +10,18 @@ i=1
 while i<=10:
     print('^'*i  )
     i+=1
+
+for item in 'Sougo':
+    print(item)
+for item in ['A',"Axz",'GG']:
+    print(item)
+for i in range(10):
+    print(i)
+for i in range(5,100,2):
+    print(i)
+prices=[10,20,40]
+total=0
+for price in prices:
+    total+=price
+print(f'Total: {total}')
+
