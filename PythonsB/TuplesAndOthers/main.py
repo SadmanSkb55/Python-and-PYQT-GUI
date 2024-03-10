4093dfb00d991005c96554771a7d730b9e884736
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2024-03-10 23:56:02 +0600

    unpacking

--- PythonsB/TuplesAndOthers/main.py
+++ PythonsB/TuplesAndOthers/main.py
@@ -4,3 +4,9 @@ print(numbers[5])
 #numbers[5]=11
 #print(numbers )
 #unpacking
+coordinates=(1,2,3)
+x,y,z=coordinates
+print(f'x:{x},y:{y},z:{z}')
+coordinates=[4,5,6]
+x,y,z=coordinates
+print(f'x:{x},y:{y},z:{z}')
