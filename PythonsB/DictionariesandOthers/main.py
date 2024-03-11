b850ca33544f0ebb5be9ab6f6264fd567bd68b33
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2024-03-11 20:05:19 +0600

    Dictionaries

---
+++ PythonsB/DictionariesandOthers/main.py
@@ -0,0 +1,36 @@
+user={
+    "Name":'Adolf Kittler',
+    "User_Name":"HitlerCar69",
+    "Password":"KillRats@1945",
+    "Age":7,
+    "is_Verified":True
+}
+print(user["User_Name"])
+print(user["Name"])
+#print(user["name"])//will show error
+print(user.get("Birthdate"))
+print(user.get("Birthdate","Jan 1,1980"))
+print(user.get("Birthdate"))
+user["Name"]="Adolf Kettlee"
+print(user["Name"])
+user["Birthdate"]="Jan 1,1980"
+print(user["Birthdate"])
+
+number=input("Number: ")
+digit_mapping={
+    "0": "Zero",
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine"
+}
+output=""
+for ch in number:
+    output+=digit_mapping.get(ch,"!")+" "
+print(output)
+
