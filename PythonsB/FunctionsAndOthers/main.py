0b14dc29956ca67ae6d1afab678e4737aca62134
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2024-03-12 15:20:19 +0600

    function reusability and so on

--- PythonsB/FunctionsAndOthers/main.py
+++ PythonsB/FunctionsAndOthers/main.py
@@ -12,3 +12,47 @@ greetuser2("Johnathan")
 username=input("Username: ")
 name=input("Name: ")
 greetuser3(name,username)
+
+#totalcalc()....u cant summon it here
+
+def total_calc(total,vat,discount):
+    print(f'Total= {total+(total*vat)-(total*discount)}')
+
+total_calc(100,0.05,0.01)
+
+total_calc(total=110,vat=0.15,discount=0.05)#keyword argument
+
+def runner(num1,num2):
+    return num1*num1*num2
+
+numx=int(input("Enter a number= "))
+cond=input("1.Square 2.Cube =")
+if cond=='1':
+     print(runner(numx,1))
+elif cond=='2':
+    print(runner(numx,numx))
+else:
+    print("Invalid")
+
+def emoji_converter(message):
+    words = message.split(' ')
+    emojis = {
+        ":)": "😀",
+        ":(": "😞",
+        ":D": "😄",
+        ":O": "😲",
+        ";)": "😉",
+        ":P": "😛",
+        "<3": "❤️",
+        ":|": "😐",
+        ":*": "😘",
+        ":/": "😕"
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word) + " "
+    return output
+
+
+message=input(">")
+print(emoji_converter(message))
