c961d1c79f4e2795a0d789b62f4cf6eb1cac6a8c
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2024-03-15 13:01:50 +0600

    Random

--- PythonsB/ModulesPAckagesAndMore/main.py
+++ PythonsB/ModulesPAckagesAndMore/main.py
@@ -8,6 +8,7 @@ Cart()
 from Ecommerce import Shipping
 Shipping.Items()
 
+import random
 
 print(Converter.kgstolbs(100))
 print(lbstokgs(233 ))
@@ -21,3 +22,15 @@ pointX=[3,4]
 pointY=[5,6]
 Dist=Converter.distance(pointX,pointY)
 print(Dist.dist())
+
+for i in range(5):
+    print(random.random())
+    print(random.randint(1,100))
+    print(random.randrange(1,100))
+    print(random.randbytes(1))
+
+
+members=['Alex','Drake','Abel','Jordan']
+leader=random.choice(members)
+print(leader)
+print(members[random.randint(0,3)]) 
