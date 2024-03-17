0adf030f8b29544810493e3ca4eacc4e704c5fee
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2024-03-18 03:40:58 +0600

    ABC

--- PythonsB/OOPsandMore/main.py
+++ PythonsB/OOPsandMore/main.py
@@ -156,3 +156,52 @@ print("Type of con2", type(con2.value))
 print(con2.value)
 print(con2.adder())
 
+#ABC(Abstract Base Classes)
+
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
+    
+print()
+rectangle = Rectangle(5, 4)
+print(rectangle.area())
+print(rectangle.perimeter())
+print()
+
+circle = Circle(3)
+print(circle.area())
+print(circle.perimeter())
+
+
+
+
+
+
